ac1a2400d7b6e782631b7914a6f0619089abc6c9
IcelandSpar <victorasielrodriguez@gmail.com>
2024-04-14 18:54:48 -0500

    Add feature to store number on calc screen
    Equal sign can now add 2 numbers when pressed if user chooses + as their operator

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let firstNumber = 0;
 let secondNumber = 0;
 let operator = '';
 
+let total = 0;
+
 
 
 
@@ -39,92 +41,146 @@ if(operator == '+') {
 }
 }
 
-const buttonZero = document.querySelector(".buttonZero")
+
+
+
 const screen = document.querySelector(".screen")
-buttonZero.addEventListener('click', () => {
-    firstNumber = 0;
-    screen.textContent = "0"
-});
+
 
 //clear
 const clear = document.querySelector(".clear")
 
 clear.addEventListener('click', () => {
     firstNumber = 0;
+    secondNumber = 0;
+    operator = '';
     screen.textContent = "";
 });
-//number
-const btn1 = document.querySelector(".btn1")
 
-btn1.addEventListener('click', () => {
-    firstNumber = parseInt(screen.textContent + "1");
-    screen.textContent = screen.textContent + "1";
+function printToScreen() {
+
+    const buttonZero = document.querySelector(".buttonZero")
+
+buttonZero.addEventListener('click', () => {
+
+    screen.textContent = screen.textContent + "0"
 });
+    //number
+    const btn1 = document.querySelector(".btn1")
+
+    btn1.addEventListener('click', () => {
+
+        screen.textContent = screen.textContent + "1";
+    });
 
 
-//number
-const btn2 = document.querySelector(".btn2")
+    //number
+    const btn2 = document.querySelector(".btn2")
 
-btn2.addEventListener('click', () => {
+    btn2.addEventListener('click', () => {
+        
+
+        screen.textContent = screen.textContent + "2";
+        
+    });
     
-    firstNumber = parseInt(screen.textContent + "2");
-    screen.textContent = screen.textContent + "2";
-    
-});
-console.log(firstNumber)
-//number
-const btn3 = document.querySelector(".btn3")
+    //number
+    const btn3 = document.querySelector(".btn3")
 
-btn3.addEventListener('click', () => {
-    firstNumber = parseInt(screen.textContent + "3");
-    screen.textContent = screen.textContent + "3";
-});
-//number
-const btn4 = document.querySelector(".btn4")
+    btn3.addEventListener('click', () => {
 
-btn4.addEventListener('click', () => {
-    firstNumber = parseInt(screen.textContent + "4");
-    screen.textContent = screen.textContent + "4";
-});
-//number
-const btn5 = document.querySelector(".btn5")
+        screen.textContent = screen.textContent + "3";
+    });
+    //number
+    const btn4 = document.querySelector(".btn4")
 
-btn5.addEventListener('click', () => {
-    firstNumber = parseInt(screen.textContent + "5");
-    screen.textContent = screen.textContent + "5";
-});
-//number
-const btn6 = document.querySelector(".btn6")
+    btn4.addEventListener('click', () => {
 
-btn6.addEventListener('click', () => {
-    firstNumber = parseInt(screen.textContent + "6");
-    screen.textContent = screen.textContent + "6";
-});
-//number
-const btn7 = document.querySelector(".btn7")
+        screen.textContent = screen.textContent + "4";
+    });
+    //number
+    const btn5 = document.querySelector(".btn5")
 
-btn7.addEventListener('click', () => {
-    firstNumber = parseInt(screen.textContent + "7");
-    screen.textContent = screen.textContent + "7";
-});
-//button
-const btn8 = document.querySelector(".btn8")
+    btn5.addEventListener('click', () => {
 
-btn8.addEventListener('click', () => {
-    firstNumber = parseInt(screen.textContent + "8");
-    screen.textContent = screen.textContent + "8";
-});
-//button
-const btn9 = document.querySelector(".btn9")
+        screen.textContent = screen.textContent + "5";
+    });
+    //number
+    const btn6 = document.querySelector(".btn6")
 
-btn9.addEventListener('click', () => {
-    firstNumber = parseInt(screen.textContent + "9");
-    screen.textContent = screen.textContent + "9";
-});
-//dot
-const dot = document.querySelector(".dot")
+    btn6.addEventListener('click', () => {
 
-dot.addEventListener('click', () => {
-firstNumber = ".";
-screen.textContent = ".";
+        screen.textContent = screen.textContent + "6";
+    });
+    //number
+    const btn7 = document.querySelector(".btn7")
+
+    btn7.addEventListener('click', () => {
+
+        screen.textContent = screen.textContent + "7";
+    });
+    //button
+    const btn8 = document.querySelector(".btn8")
+
+    btn8.addEventListener('click', () => {
+
+        screen.textContent = screen.textContent + "8";
+    });
+    //button
+    const btn9 = document.querySelector(".btn9")
+
+    btn9.addEventListener('click', () => {
+
+        screen.textContent = screen.textContent + "9";
+    });
+    //dot
+    const dot = document.querySelector(".dot")
+
+    dot.addEventListener('click', () => {
+
+    screen.textContent = screen.textContent + ".";
+    });
+  
+} 
+
+function getScreenNumber() {
+    return screen.textContent
+}
+
+
+
+function clearTextArea() {
+    screen.textContent = '';
+}
+
+
+
+function getOperator() {
+
+    const plusBtn = document.querySelector(".plusBtn");
+
+    plusBtn.addEventListener('click', () => {
+        firstNumber = parseFloat(screen.textContent);
+        operator = '+'
+        screen.textContent = "+";
+    });
+    return operator
+}
+
+
+
+
+printToScreen();
+
+getOperator();
+
+
+const totalBtn = document.querySelector(".totalBtn")
+
+totalBtn.addEventListener('click', () => {
+    secondNumber = parseFloat(screen.textContent);
+    total = operate(firstNumber, secondNumber, operator);
+    screen.textContent = total;
 });
+
+
